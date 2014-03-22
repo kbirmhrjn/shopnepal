77d0b653732ff9f814278a23e114a4e5eb6e04d0
kabir maharjan <kabeerdarocker@gmail.com>
2014-03-22 21:47:11 +0545

    app.php (local  + prod)

--- app/config/app.php
+++ app/config/app.php
@@ -65,7 +65,7 @@ return array(
 	|
 	*/
 
-	'key' => 'YourSecretKey!!!',
+	'key' => 'mMoJUbRIqWaCIsKPpO7xdDDYC1a1a7dM',
 
 	/*
 	|--------------------------------------------------------------------------
--- app/config/local/app.php
+++ app/config/local/app.php
@@ -1,2 +1,30 @@
-<?php 
-return ['debug' => true];
+<?php
+
+return array(
+
+    'debug' => true,
+    /*
+    |--------------------------------------------------------------------------
+    | Append environment service providers
+    |--------------------------------------------------------------------------
+    */
+
+    'providers' => append_config(array(
+
+        'Clockwork\Support\Laravel\ClockworkServiceProvider',
+
+    )),
+
+    /*
+    |--------------------------------------------------------------------------
+    |  Append environment aliases
+    |--------------------------------------------------------------------------
+    */
+
+    'aliases' => append_config(array(
+
+        'Clockwork'       => 'Clockwork\Support\Laravel\Facade',
+
+    )),
+
+);
