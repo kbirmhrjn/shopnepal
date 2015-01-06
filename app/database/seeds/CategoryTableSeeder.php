5c9e64d0f9b7217ca06ac2ecbdeced2db15a2a2c
kabir maharjan <kabeerdarocker@gmail.com>
2015-01-06 16:52:13 +0545

    updated categorytable seeder

--- app/database/seeds/CategoryTableSeeder.php
+++ app/database/seeds/CategoryTableSeeder.php
@@ -1,6 +1,9 @@
 <?php
+use Faker\Generator;
 use Shop\Categories\Category;
 use Faker\Factory as Faker;
+use Shop\Question;
+
 class CategoryTableSeeder extends Seeder {
 
 	public function run()
@@ -9,11 +12,36 @@ class CategoryTableSeeder extends Seeder {
         $faker = Faker::create();
         foreach (range(1, 25) as $index)
         {
-            Category::create([
+            $category = Category::create([
                 'title' => $faker->word,
                 'body'  => $faker->paragraph(5),
             ]);
+            $this->makeQuestion($faker, $category);
         }
 	}
+
+    /**
+     * Make question According to its category
+     * @param Generator $faker
+     * @param Category $category
+     */
+    private function makeQuestion( Generator $faker,Category $category)
+    {
+        $type = ['user', 'select'];
+        foreach(range(1, 10) as $index)
+        {
+            $typeOf = array_rand($type);
+            $question = [
+                'title' => str_replace('.',' ?', $faker->sentence),
+                'type' => $type[$typeOf],
+                'options' => $type[$typeOf] == 'select'
+                    ? json_encode(['option1','option2','option3'])
+                    : null,
+            ];
+            $question = Question::create($question);
+            $category->questions()->save($question);
+        }
+    }
+
 
 }
